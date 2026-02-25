299172660a092530df12085e5a6baf0a6daf270a
tummie <tumikef@gmail.com>
2026-02-25 10:00:55 -0500

    corrected the failed build due to routes having spaces

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -171,3 +171,4 @@ class TestAccountService(TestCase):
         """It should not allow an illegal method call"""
         resp = self.client.delete(BASE_URL)
         self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
